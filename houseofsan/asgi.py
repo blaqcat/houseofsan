44d90675245c4e2a82c6ef593ae9ac95abcd34f3
blaqcat <ngakana@protonmail.com>
2021-09-20 09:52:31 +0200

    first commit

---
+++ houseofsan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for houseofsan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'houseofsan.settings')
+
+application = get_asgi_application()
